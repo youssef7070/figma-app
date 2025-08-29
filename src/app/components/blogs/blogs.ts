3183c772200f171dc7f2e5875bd9e8c49d73a0b5
youssef7070 <youssefmo7amed7070@gamil.com>
2025-08-29 18:29:37 +0300

    Fix images path - move to assets

--- src/app/components/blogs/blogs.ts
+++ src/app/components/blogs/blogs.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './blogs.css'
 })
 export class Blogs {
-  image1: string = "/public/works&blocks/1.jpg;"
-  image2: string = "works&blocks/2.jpg";
+  image1: string = "/works&blocks/1.jpg";
+  image2: string = "/works&blocks/2.jpg";
   image3: string = "/works&blocks/3.jpg";
   image4: string = "/works&blocks/4.jpg";
 
